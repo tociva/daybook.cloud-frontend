367e938401e854f0d72e228a05863bd632cdc9a8
Prince Francis <princekfrancis@gmail.com>
2025-07-28 18:34:09 +0530

    expandable menu when collapse.

--- src/app/features/layout/side-bar-content/side-bar-content.ts
+++ src/app/features/layout/side-bar-content/side-bar-content.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { menuList } from '../../../util/menu-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-side-bar-content',
@@ -15,7 +16,8 @@ import { menuList } from '../../../util/menu-list';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    RouterLink
+    RouterLink,
+    MatTooltipModule
     ],
   templateUrl: './side-bar-content.html',
   styleUrl: './side-bar-content.scss'
@@ -27,6 +29,11 @@ export class SideBarContent {
 
   menuList = menuList;
 
+  onMenuIconClick(trigger: MatMenuTrigger) {
+    // Optional: check logic, or just open
+    trigger.openMenu();
+  }
+  
   
   toggleSubmenu(name: string): void {
     if (this.openSubmenus.has(name)) {
@@ -40,4 +47,7 @@ export class SideBarContent {
     return this.openSubmenus.has(name);
   }
 
+  showSubmenu(name: string): void {
+    console.log(name);
+  }
 }
