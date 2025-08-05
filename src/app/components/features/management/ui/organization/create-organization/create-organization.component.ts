c3509009abdede0ae66c2dfb0b7677789edf84b0
Prince Francis <princekfrancis@gmail.com>
2025-08-05 11:46:27 +0530

    Dynamic form crreation : started.

--- src/app/components/features/management/ui/organization/create-organization/create-organization.component.ts
+++ src/app/components/features/management/ui/organization/create-organization/create-organization.component.ts
@@ -1,42 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { TwoColumnFormComponent } from '../../../../../shared/forms/two-column-form/two-column-form.component';
 
 @Component({
   selector: 'app-create-organization',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [TwoColumnFormComponent],
   templateUrl: './create-organization.component.html',
   styleUrl: './create-organization.component.css'
 })
 export class CreateOrganizationComponent {
-  private fb = inject(FormBuilder);
-
-  form: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    mobile: [''],
-    address: this.fb.group({
-      line1: [''],
-      line2: [''],
-      city: [''],
-      pincode: [''],
-    }),
-    description: [''],
-    country: [null],
-    state: [''],
-    fiscalstart: ['', Validators.required],
-    fiscalname: ['', Validators.required],
-    startdate: ['', Validators.required],
-    enddate: ['', Validators.required],
-    gstin: [''],
-    invnumber: [''],
-    currency: [null],
-    jnumber: [''],
-  });
-
-  submitForm() {
-    if (this.form.valid) {
-      console.log(this.form.value);
-    }
-  }
+  
 }
---
+++ src/app/components/shared/forms/two-column-form/two-column-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-column-form',
+  imports: [],
+  templateUrl: './two-column-form.component.html',
+  styleUrl: './two-column-form.component.css'
+})
+export class TwoColumnFormComponent {
+
+}
