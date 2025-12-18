87bbf2cba89ad3af94170f6231f6f084cd9564a4
Abin Mathew <abin02066@gmail.com>
2025-12-18 12:08:54 +0530

    Fix date picker bugs, form errors, and update README config

--- src/app/components/shared/dbc-date-picker/dbc-date-picker.ts
+++ src/app/components/shared/dbc-date-picker/dbc-date-picker.ts
@@ -7,11 +7,12 @@ import {
   forwardRef,
   HostListener,
   inject,
+  Input,
   input,
   signal,
   viewChild,
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 
@@ -31,7 +32,7 @@ dayjs.extend(customParseFormat);
     },
   ],
 })
-export class DbcDatePicker {
+export class DbcDatePicker implements ControlValueAccessor {
 
   selectedDate = signal<Date | null>(null);
   draftDate = signal<Date | null>(null);
@@ -40,83 +41,110 @@ export class DbcDatePicker {
   showCalendar = signal<boolean>(false);
   isDisabled = signal<boolean>(false);
 
-  inputDay = signal('');
-  inputMonth = signal('');
-  inputYear = signal('');
+  dayValue = signal('');
+  monthValue = signal('');
+  yearValue = signal('');
 
   format = input<string>('DD/MM/YYYY');
 
-  
-   readonly months = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+  dayPlaceholderText = computed(() => 'DD');
+  yearPlaceholderText = computed(() => 'YYYY');
+  monthPlaceholderText = computed(() => {
+    const f = this.format();
+    if (f.includes('MMM')) return 'MMM';
+    if (f.includes('MMMM')) return 'MMMM';
+    return 'MM';
+  });
+
+
+  readonly months = [
+    'Jan', 'Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',
   ];
 
   readonly yearOptions = Array.from({ length: 1101 }, (_, i) => 1900 + i);
 
   yearList = viewChild<ElementRef<HTMLDivElement>>('yearList');
 
-  calendarDays = computed(() =>
-    this.getCalendarDays(this.currentYear(), this.currentMonth())
-  );
+  calendarDays = computed(() => this.getCalendarDays(this.currentYear(), this.currentMonth()));
 
   private onChange: (value: Date | null) => void = () => {};
   private onTouched: () => void = () => {};
 
   private elem = inject(ElementRef);
- private inputEffect = effect(() => {
-  const dayStr =Number(this.inputDay());
-  const monthStr =Number(this.inputMonth());
-  const yearStr = Number(this.inputYear());
+  private inputEffect = effect(() => {
+    const day = Number(this.dayValue());
+    const monthIndex = this.parseMonthToIndex(this.monthValue());
+    const year = Number(this.yearValue());
 
-this.draftDate.set(null);
+    this.draftDate.set(null);
 
-if (dayStr) {
-    const day = Number(dayStr);
-    if (isNaN(day) || day < 1 || day > 31) {
-      this.inputDay.set('');
+    if (day) {
+      if (isNaN(day) || day < 1 || day > 31) {
+        this.dayValue.set('');
+        return;
+      }
+    }
+    if (this.monthValue() && monthIndex === null) {
+      this.monthValue.set('');
       return;
     }
-  }
- if (monthStr) {
-    const month = Number(monthStr);
-    if (isNaN(month) || month < 1 || month > 12) {
-      this.inputMonth.set('');
+    if (year) {
+      const yearstr = Number(year);
+
+      if (isNaN(yearstr) || yearstr < 1900 || yearstr > 3000) {
+        this.yearValue.set('');
+        return;
+      }
+    }
+
+    if (!day || monthIndex === null || !year) return;
+
+    const date = new Date(year, monthIndex, day);
+
+    if (date.getDate() !== day && date.getFullYear()! == year ) {
+      this.resetDateInputs();
       return;
     }
-  }  
-if (yearStr) {
-    const year = Number(yearStr);
-    if (isNaN(year) || year < 1900 || year > 3000) {
-      this.inputYear.set('');
-      return;
-    }
-  }
-  if (!dayStr || !monthStr || !yearStr) {
-    return;
+    this.selectedDate.set(date);
+    this.draftDate.set(date);
+    this.currentMonth.set(monthIndex);
+    this.currentYear.set(year);
+  });
+
+  // get dateSeparator(): string {
+  //   const match = this.format().match(/[^A-Za-z0-9]/);
+  //   return match ? match[0] : '/';
+  // }
+  formatParts = computed(()=>
+    this.format().match(/[A-Za-z]+|[^A-Za-z]/g) ?? []
+  );
+
+  private resetDateInputs() {
+    this.dayValue.set('');
+    this.monthValue.set('');
+    this.yearValue.set('');
+    this.draftDate.set(null);
   }
 
-  const day = Number(dayStr);
-  const month = Number(monthStr);
-  const year = Number(yearStr);
-
-  const date = dayjs(`${day}-${month}-${year}`, 'D-M-YYYY', true);
-
-  if (!date.isValid()) {
-    this.resetInputs();
-    return;
-  }
-  this.draftDate.set(date.toDate());
-  this.currentMonth.set(date.month());
-  this.currentYear.set(date.year());
+  monthMaxLength = computed(() => {
+  const f = this.format();
+  if (f.includes('MMM')) return 3;
+  return 2;                        
 });
 
-private resetInputs() {
-  this.inputDay.set('');
-  this.inputMonth.set('');
-  this.inputYear.set('');
-  this.draftDate.set(null);
-}
+
+  private parseMonthToIndex(value: string): number | null {
+    if (!value) return null;
+
+    const lower = value.toLowerCase();
+    if (/^\d+$/.test(lower)) {
+      const num = Number(lower);
+      return num >= 1 && num <= 12 ? num - 1 : null;
+    }
+    const index = this.months.map((m) => m.toLowerCase()).findIndex((m) => m.startsWith(lower));
+
+    return index !== -1 ? index : null;
+  }
 
   @HostListener('document:click', ['$event'])
   handleDocumentClick(event: MouseEvent) {
@@ -165,7 +193,6 @@ private resetInputs() {
     this.updateDraftDate(this.currentMonth(), year);
   }
 
-
   writeValue(value: Date | string | null): void {
     if (!value) return;
     const date = value instanceof Date ? value : this.parseFormattedDate(value);
@@ -210,23 +237,31 @@ private resetInputs() {
   }
 
   isSelected(day: number | null): boolean {
-    const selected = this.selectedDate();
-    if (!selected || !day) return false;
+    const draftDate = this.draftDate();
+    if (!draftDate || !day) return false;
     return (
-      selected.getDate() === day &&
-      selected.getMonth() === this.currentMonth() &&
-      selected.getFullYear() === this.currentYear()
+      draftDate.getDate() === day &&
+      draftDate.getMonth() === this.currentMonth() &&
+      draftDate.getFullYear() === this.currentYear()
     );
   }
 
-
   private setSelectedDate(date: Date) {
     this.selectedDate.set(date);
-    this.inputDay.set(String(date.getDate()).padStart(2, '0'));
-    this.inputMonth.set(String(date.getMonth() + 1).padStart(2, '0'));
-    this.inputYear.set(date.getFullYear().toString());
+
+    const d = dayjs(date);
+    const parts = d.format(this.format()).match(/[A-Za-z]+|\d+/g) ?? [];
+
+    const formatParts = this.format().match(/[A-Za-z]+/g) ?? [];
+
+    formatParts.forEach((token, i) => {
+      if (token.startsWith('D')) this.dayValue.set(parts[i]);
+      if (token.startsWith('M')) this.monthValue.set(parts[i]);
+      if (token.startsWith('Y')) this.yearValue.set(parts[i]);
+    });
+
     this.draftDate.set(date);
-    this.updateForm(date);
+    this.emitValueChange(date);
   }
 
   private syncCalendar(date: Date) {
@@ -235,24 +270,20 @@ private resetInputs() {
   }
 
   private updateDraftDate(month: number, year: number) {
-    const base = this.draftDate() ?? new Date();
+    const selected = this.selectedDate();
+    const day = selected?.getDate() ?? 1;
     const maxDay = new Date(year, month + 1, 0).getDate();
-    this.draftDate.set(new Date(year, month, Math.min(base.getDate(), maxDay)));
+    this.draftDate.set(new Date(year, month, Math.min(day, maxDay)));
   }
 
   private getCalendarDays(year: number, month: number): (number | null)[] {
     const firstWeekday = new Date(year, month, 1).getDay();
     const lastDay = new Date(year, month + 1, 0).getDate();
-    return [
-      ...Array(firstWeekday).fill(null),
-      ...Array.from({ length: lastDay }, (_, i) => i + 1),
-    ];
+    return [...Array(firstWeekday).fill(null), ...Array.from({ length: lastDay }, (_, i) => i + 1)];
   }
 
-  private updateForm(date: Date) {
+  private emitValueChange(date: Date) {
     this.onChange(date);
-    console.log(date);
-    
     this.onTouched();
   }
 }
